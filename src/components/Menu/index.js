be9d27ee044ee5a0880e6c0614cd44e2b64cf591
Almir Lopes <almir.junior@triasoftware.com.br>
2020-07-27 16:41:55 -0300

    first commit

---
+++ src/components/Menu/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Logo from "../../assets/images/almirflix.png";
+import "./Menu.css";
+import Button from "../Button"
+
+
+function Menu() {
+  return (
+    <nav className="Menu">
+      <a href="/">
+        <img className="Logo" src={Logo} alt="AluraFlix logo" />
+      </a>
+
+      <Button as="a" className="ButtonLink" href="/">
+        Novo Vídeo
+      </Button>
+    </nav>
+  );
+}
+
+export default Menu;
